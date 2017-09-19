921d1eed23daac3dda3783e78871e71576b5c393
suchol4 <jakubdynowski1993@gmail.com>
2017-09-19 22:50:04 +0200

    Initial commit

---
+++ Library/src/src/app/LibraryApp.java
@@ -0,0 +1,11 @@
+package src.app;
+
+public class LibraryApp {
+	public static final String APP_NAME = "Library made by Jakub Dynowski with coorp JavaStart";
+
+	public static void main(String args[]) {
+		System.out.println(APP_NAME);
+		LibraryControl libControl = new LibraryControl();
+		libControl.controlLoop();
+	}
+}
---
+++ Library/src/src/utils/LibraryUtils.java
@@ -0,0 +1,33 @@
+package src.utils;
+
+import src.data.Book;
+import src.data.Library;
+import src.data.Magazine;
+
+public class LibraryUtils {
+    
+    public static void printBooks(Library lib) {
+        printPublications(lib,Book.class);
+    }
+    
+    //.filter(Magazine.class::isInstance)
+    //.filter(x -> x instanceof Magazine) to samo co wy¿ej
+    public static void printMagazines(Library lib) {
+        printPublications(lib,Magazine.class);
+    }
+    
+    private static void printPublications(Library lib,Class cl){
+    	long countMagazines = lib.getPublications().values().stream()
+    			.filter(cl::isInstance).sorted(new Library.AlphabeticalComparator())
+    			.peek(System.out::println).count();
+    	if(countMagazines == 0){
+    		System.out.println("W bibliotece nie znaleziono publikacji typu " + cl.getSimpleName());
+    	}
+    }
+    
+    public static void printUsers(Library lib) {
+    	lib.getUsers().values().stream()
+    					.sorted((a,b)->a.getLastName().compareTo(b.getLastName()))
+    					.forEach(System.out::println);
+    }
+}
